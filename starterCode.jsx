d63ab2df4645768dd2b516c0814435b093450e65
Harriet Ebozele <105316823+harrietebozele@users.noreply.github.com>
2023-02-03 17:58:16 -0500

    Add files via upload

---
+++ starterCode.jsx
@@ -0,0 +1,48 @@
+
+  const ATMDeposit = ({onChange, isDeposit}) => {
+    const choice = ['Deposit', 'Cash Back'];
+ 
+  return (
+    <label className="label huge">
+      <h3>{choice[Number(!isDeposit)]}</h3>
+  
+      <input type="number" width = '200' onChange = {onChange}></input>
+      <input type="submit" width = '200' value = 'Submit'></input>
+      
+      
+    </label>
+  );
+};
+
+const Account = () => {
+  let transactionState = 0; // state of this transaction
+const [totalState, setTotalState] = React.useState(0);
+const [isDeposit, setIsDeposit] = React.useState(true);
+  let status = `Account Balance $ ${totalState}`;
+  console.log('Account Rendered');
+  const handleChange = event => {
+    console.log(`handleChange ${event.target.value}`);
+    transactionState = Number(event.target.value);
+  };
+  const handleSubmit = ()=> {
+  let newTotal = isDeposit ? totalState + transactionState : totalState - transactionState ;
+  if(newTotal<0){
+    alert('Overdraft');
+  }
+  
+    setTotalState(newTotal);
+    event.preventDefault();
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2 id = 'total'>{status}</h2>
+      
+      <button onClick = {()=>setIsDeposit(true)}>Deposit</button>
+      <button onClick = {()=>setIsDeposit(false)}>Cash Back</button>
+      
+      <ATMDeposit onChange={handleChange} isDeposit = {isDeposit}> Deposit</ATMDeposit>
+    </form>
+  );
+};
+// ========================================
+ReactDOM.render(<Account />, document.getElementById("root"));
